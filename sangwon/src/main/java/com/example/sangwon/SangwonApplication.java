28968592525c6be815a38494fc7800928f312541
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-10-19 14:07:20 +0900

    innit

---
+++ sangwon/src/main/java/com/example/sangwon/SangwonApplication.java
@@ -0,0 +1,13 @@
+package com.example.sangwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangwonApplication.class, args);
+	}
+
+}
---
+++ sangwon/src/test/java/com/example/sangwon/SangwonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sangwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
