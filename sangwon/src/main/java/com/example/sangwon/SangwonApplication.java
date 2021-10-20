a45205bafa4a38ccd5f8892ed4cb1285ac9f64ab
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-10-20 17:41:10 +0900

    reset

--- sangwon/src/main/java/com/example/sangwon/SangwonApplication.java
+++ sangwon/src/main/java/com/example/sangwon/SangwonApplication.java
@@ -2,12 +2,20 @@ package com.example.sangwon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SangwonApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/sangwon/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(SangwonApplication.class, args);
+		new SpringApplicationBuilder(SangwonApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+//		SpringApplication.run(SangwonApplication.class, args);
 	}
 
 }
