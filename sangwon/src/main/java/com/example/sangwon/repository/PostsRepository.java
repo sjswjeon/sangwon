f87c048b9e3dd1b844e8f04c83383467fe5fd0e1
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-10-23 16:25:19 +0900

    test....

--- sangwon/src/main/java/com/example/sangwon/repository/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sangwon.repository;
-
-import com.example.sangwon.Model.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
