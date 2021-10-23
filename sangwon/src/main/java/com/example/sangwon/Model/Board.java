5e82fc5223ab3df9edfbf7250f6bf8578bf6ca40
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-10-23 19:51:29 +0900

    test....

--- sangwon/src/main/java/com/example/sangwon/Model/Board.java
+++ sangwon/src/main/java/com/example/sangwon/Model/Board.java
@@ -35,6 +35,6 @@ public class Board {
 //            inverseJoinColumns = @JoinColumn(name = "user_id"))
 //    List<User> likedUsers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "likedBoard", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "likedBoard")
     private List<User> likedUsers = new ArrayList<>();
 }
