6fddfc86752c26e76e6e723f82d9da5474c81104
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-10-24 12:42:42 +0900

    test....

--- sangwon/src/main/java/com/example/sangwon/Model/Board.java
+++ sangwon/src/main/java/com/example/sangwon/Model/Board.java
@@ -24,7 +24,7 @@ public class Board {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "board")
+    @OneToMany(mappedBy = "board", fetch =FetchType.LAZY)
     private List<Comment> comments = new ArrayList<>();
 
 
