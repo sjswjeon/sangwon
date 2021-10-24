dcc9b3248428b8541b81dab49311951ddd3c5115
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-10-24 14:49:40 +0900

    test....

--- sangwon/src/main/java/com/example/sangwon/Model/UserBoard.java
+++ sangwon/src/main/java/com/example/sangwon/Model/UserBoard.java
@@ -3,12 +3,15 @@ package com.example.sangwon.Model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class UserBoard {
 
+    @Id
     private Long board_id;
 
+    @Id
     private Long user_id;
 }
